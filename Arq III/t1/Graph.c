7be0a5966b810a5f93b45c2a1e7950a311fa0a34
Matthias Nunes <matthiasnunes@gmail.com>
2014-05-21 05:03:35 -0300

    run.sh finished, just need to run the tests

--- Arq III/t1/Graph.c
+++ Arq III/t1/Graph.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<LinkedList.h>
 
-#define MAX 3000
+#define MAX 10000
 #define SEED 1
 
 unsigned char graph[MAX][MAX];
@@ -15,16 +15,15 @@ unsigned int sum;
 void crazyPath(int node)
 {
 	if (marks[node])
-	{
 		return;
-	}
+
 	marks[node] = 1;
+	sum += getVal(&nodes, node);
 	int i = 0;
 	for (i = 0; i < MAX; i++)
 	{
 		if (graph[node][i])
 		{
-			sum += getVal(&nodes, i);
 			crazyPath(i);
 		}
 	}
@@ -33,12 +32,16 @@ void crazyPath(int node)
 int main(int argc, const char *argv[])
 {
 	srand(SEED);
+
 	nodes.head = NULL;
 	nodes.current = NULL;
 	nodes.size = 0;
+
 	sum = 0;
+
 	int i = 0;
 	int j = 0;
+
 	printf("\nGenerating connections...");
 	for (i = 0; i < MAX; i++)
 	{
@@ -47,14 +50,16 @@ int main(int argc, const char *argv[])
 			graph[j][i] = rand() % 2;
 		}
 		marks[i] = 0;
-		//printf(".");
 		nodes.head = addToList(&nodes, rand() % MAX, true);
 	}
 	printf("DONE!!\n");
+
 	printf("\nStarting crazyPath...");
 	crazyPath(0);
 	printf("DONE!!\n");
+
 	destroy(&nodes);
+
 	printf("Sum = %d\n", sum);
 	return 0;
 }

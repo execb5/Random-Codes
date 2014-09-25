38a92ea0be6e5a13eb94f266ec0b2c6fc34125f8
Matthias Nunes <matthiasnunes@gmail.com>
2014-09-25 04:04:06 -0300

    Updates baby.hs

--- baby.hs
+++ baby.hs
@@ -17,8 +17,21 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 length' xs = sum [1 | _ <- xs]
 
+removeNonUppercase :: String -> String
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
 
 rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r

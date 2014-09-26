0faf501bfbd9a1d297ad1340f44f67d0f6d8492a
Matthias Nunes <matthiasnunes@gmail.com>
2014-09-26 14:44:12 -0300

    Tabs corrected

--- baby.hs
+++ baby.hs
@@ -151,25 +151,25 @@ bmiTell weight height
 
 max' :: (Ord a) => a -> a -> a
 max' a b
-        | a > b     = a
-        | otherwise = b
+    | a > b     = a
+    | otherwise = b
 {-In line using guards. Advised against because it's less readable.-}
 {-max' a b | a > b = a | otherwise = b-}
 
 myCompare :: (Ord a) => a -> a -> Ordering
 a `myCompare` b
-        | a > b     = GT
-        | a == b    = EQ
-        | otherwise = LT
+    | a > b     = GT
+    | a == b    = EQ
+    | otherwise = LT
 
 initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
-        where (f:_) = firstname
-              (l:_) = lastname
+    where (f:_) = firstname
+          (l:_) = lastname
 
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]
 calcBmis xs = [bmi w h | (w, h) <- xs]
-        where bmi weight height = weight / height ^ 2
+    where bmi weight height = weight / height ^ 2
 {-Writing the same function using let binding-}
 {-calcBmis :: (RealFloat a) => [(a, a)] -> [a]-}
 {-calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]-}
@@ -184,9 +184,9 @@ calcBmis xs = [bmi w h | (w, h) <- xs]
 {-cylinder's surface area based on its height and radius: -}
 cylinder :: (RealFloat a) => a -> a -> a
 cylinder r h =
-        let sideArea = 2 * pi * r * h
-            topArea = pi * r ^ 2
-        in sideArea + 2 * topArea
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
 
 {-Case expressions-}
 describeList :: [a] -> String

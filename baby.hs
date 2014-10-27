0c0dce78e2b72f4853d7053d646ae10e3b732279
Matthias Nunes <matthiasnunes@gmail.com>
2014-10-27 04:08:25 -0200

    Done until Data.Set subsection on chapter 7 of the book.

--- baby.hs
+++ baby.hs
@@ -7,9 +7,11 @@
 
 --imports... only needed after chapter 7
 import Data.List
+import Data.Char
 --import Data.List (nub,sort) use like this to import only those functions
 --import Data.List hiding (nub) import all functions except nub
-import qualified Data.Map as M
+import qualified Data.Map as Map
+import qualified Data.Set as Set
 --qualified makes us reference Data.Map when we call its functions
 --as M is just a rename so that we don't have to type Data.Map everytime
 
@@ -444,3 +446,42 @@ search :: (Eq a) => [a] -> [a] -> Bool
 search needle haystack =
     let nlen = length needle
     in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
+
+encode :: Int -> String -> String
+encode shift msg =
+    let ords = map ord msg
+        shifted = map (+ shift) ords
+    in map chr shifted
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
+phoneBook = 
+    [
+        ("betty", "555-2938"),
+        ("betty","342-2492"),
+        ("bonnie", "452-2928"),
+        ("patsy","493-2928"),
+        ("patsy","943-2929"),
+        ("patsy","827-9162"),
+        ("lucille","205-2928"),
+        ("wendy","939-8282"),
+        ("penny","853-2492"),
+        ("penny","555-2111")
+    ]
+
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey key [] = Nothing
+findKey key ((k,v):xs) = if key == k then Just v else findKey key xs
+
+findKey' :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey' key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
+
+fromList' :: (Ord k) => [(k,v)] -> Map.Map k v
+fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap xs = Map.fromListWith (\number1 number2 -> number1 ++ ", " ++ number2) xs
+
+phoneBookToMap' :: (Ord k) => [(k, a)] -> Map.Map k [a]  
+phoneBookToMap' xs = Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs

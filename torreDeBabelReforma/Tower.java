b52d7d683faca85905c972938aaf391d23361bb9
Matthias Nunes <matthiasnunes@gmail.com>
2014-10-30 17:01:13 -0200

    Not working... yet!

--- torreDeBabelReforma/Tower.java
+++ torreDeBabelReforma/Tower.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
+
 public class Tower {
         private int height;
         private boolean[][] paths;
+        private boolean[] marks;
 
         public Tower(int n) {
                 height = n;
                 paths = new boolean[height][height];
+                marks = new boolean[height];
         }
 
         public void insertPath(int floor1, int floor2) {
                 paths[floor1][floor2] = true;
         }
+
 
         public void printMatrix() {
                 for (int i = 0; i < height; i++) {
@@ -19,11 +24,30 @@ public class Tower {
                 }
         }
 
-        public boolean isItPossible(int whereIam, int whereIWannaGo) {
-                return false;
+        public boolean isItPossible(int from, int to) {
+                if (from == to)
+                        return true;
+                marks[from] = true;
+                boolean possibility = false;
+                for (Integer neighbor : getNeighbors(from)) {
+                        if (!marks[neighbor])
+                                possibility = possibility ||
+                                        isItPossible(neighbor, to);
+                }
+                return possibility;
+        }
+
+        private ArrayList<Integer> getNeighbors(int currentNode) {
+                ArrayList<Integer> l = new ArrayList<Integer>(height);
+                for (int i = 0; i < height; i++) {
+                        if (paths[currentNode][i])
+                                l.add(i);
+                }
+                return l;
         }
 
         public int getHeight(){
                 return this.height;
         }
 }
+

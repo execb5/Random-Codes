892e3fda072d3bd911e75f243a50263163aef21a
Matthias Nunes <matthiasnunes@gmail.com>
2016-11-01 21:37:34 -0200

    Adds shit :goberserk:

---
+++ sleep_sort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int c, char **v)
+{
+	while (--c > 1 && !fork());
+	usleep(c = atoi(v[c]));
+	printf("%d\n", c);
+	wait(0);
+	return 0;
+}
